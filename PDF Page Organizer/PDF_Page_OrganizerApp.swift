1022705071d50caaca5c711d9a8ca487a6657fc9
fenuku kekeli <fenuku.kekeli8989@gmail.com>
2026-02-03 13:57:02 +0000

    Initial Commit

---
+++ PDF Page Organizer/PDF_Page_OrganizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDF_Page_OrganizerApp.swift
+//  PDF Page Organizer
+//
+//  Created by Fenuku kekeli on 2/3/26.
+//
+
+import SwiftUI
+
+@main
+struct PDF_Page_OrganizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
